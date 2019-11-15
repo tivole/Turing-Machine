8b6e02e1b88adfba46e9fba84e1cb0dbf982e8b1
tivole <asgarovkamran@gmail.com>
2019-11-15 23:23:53 +0400

    Wrote Turing_Machine function

--- Compiler/compiler.py
+++ Compiler/compiler.py
@@ -5,6 +5,16 @@
 
 """
 
+"""
+    TODO list:
+
+    1) TODO: Check syntax correction.
+
+"""
+
+# Libraries
+import copy
+
 __code__ = open('turing_code.txt', 'r')
 
 __lines__ = __code__.readlines()
@@ -19,14 +29,51 @@ __NUMBER_OF_EMPTY__ = int(__lines__[1][__lines__[1].find('{') + 1:__lines__[1].f
 
 # Reading codes
 __CODE__ = []
-for __i__ in range(1, len(__lines__)):
+for __i__ in range(2, len(__lines__)):
     __CODE__.append(tuple(__lines__[__i__][:__lines__[__i__].find(';')].replace(' ', '').replace('->', ',').split(',')))
 
 
-def Turing_Machine(Sequence):
+def Turing_Machine(Sequence_INPUT):
     """
         Function to program Turing Machine code.
     """
-    Sequence = __EMPTY__ * __NUMBER_OF_EMPTY__ + Sequence + __EMPTY__ * __NUMBER_OF_EMPTY__
+    Sequence_INPUT = list(__EMPTY__ * __NUMBER_OF_EMPTY__ + Sequence_INPUT + __EMPTY__ * __NUMBER_OF_EMPTY__)
+    Sequence = copy.deepcopy(Sequence_INPUT)
     
-    return Sequence
+    state = 'q1' # The first state must be 'q1' TODO: check in syntax
+
+    machine_arrow = 0
+    # Put the machine_arrow in the right place
+    while Sequence_INPUT[machine_arrow] == __EMPTY__:
+        machine_arrow += 1
+
+    Sequences_OUTPUT = []
+
+    Sequences_OUTPUT.append([copy.deepcopy(Sequence_INPUT), state, machine_arrow, ''])
+
+    TM_STOP = False
+
+    
+
+    while not TM_STOP:
+        for i in range(len(__CODE__)):
+            STOP = False
+            if state == __CODE__[i][0] and Sequence[machine_arrow] == __CODE__[i][1]:
+                Sequence[machine_arrow] = __CODE__[i][3]
+                state = __CODE__[i][2]
+                if __CODE__[i][4] == 'R':
+                    machine_arrow += 1 # Go RIGHT
+                elif __CODE__[i][4] == 'L':
+                    machine_arrow -= 1 # Go LEFT
+                elif __CODE__[i][4] == 'S':
+                    machine_arrow = machine_arrow  # STOP
+                    STOP = True
+
+            Sequences_OUTPUT.append([copy.deepcopy(Sequence), state, machine_arrow, __CODE__[i][4]])
+
+            if state == 'q0' and STOP == True:
+                TM_STOP = True
+
+
+
+    return Sequences_OUTPUT
--- Compiler/main.py
+++ Compiler/main.py
@@ -3,3 +3,5 @@ import compiler
 input_s = open('input.txt', 'r')
 
 Sequence = input_s.readline()
+
+Sequences_OUTPUT = compiler.Turing_Machine(Sequence)
